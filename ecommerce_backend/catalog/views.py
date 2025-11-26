57f59b43cc5338fb23c463153afab2a7ad5909bf
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-26 22:36:46 +0700

    feat: Added filtering queryset to filter catalog
    using get_queryset, API front end can now filter product by availability and optionally by category.
    Query params: ?category=<slug>

--- ecommerce_backend/catalog/views.py
+++ ecommerce_backend/catalog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
@@ -6,13 +5,27 @@ from .serializers import CategorySerializer, ProductSerializer
 # using pre-defined views from rest_framework
 
 class CategoryListView(generics.ListAPIView):
-    #API endpoint to list all product categories.
+    """API endpoint to list all product categories."""
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
 class ProductListCreateView(generics.ListCreateAPIView):
-    #API endpoint to list and create products.
-    # queryset = Product.objects.all()
-    # instead of listing all products I will list only those that are available
-    queryset = Product.objects.filter(is_available=True)
+    """API endpoint to list and create products."""
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        """
+        Filter products by availability and optionally by category.
+        Query params: ?category=<slug>
+        """
+        # Start with only available products
+        queryset = Product.objects.filter(is_available=True)
+        
+        # Optionally filter by category slug
+        category_slug = self.request.query_params.get('category')
+        if category_slug:
+            queryset = queryset.filter(category__slug=category_slug)
+        
+        return queryset
+
