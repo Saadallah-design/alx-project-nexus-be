7c166e3965b685bc9fe678454f9ac3605c769aa3
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-26 22:26:45 +0700

    feat: adding product and catalog views | products queryset by availability

--- ecommerce_backend/catalog/views.py
+++ ecommerce_backend/catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
-# Create your views here.
+# using pre-defined views from rest_framework
+
+class CategoryListView(generics.ListAPIView):
+    #API endpoint to list all product categories.
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class ProductListCreateView(generics.ListCreateAPIView):
+    #API endpoint to list and create products.
+    # queryset = Product.objects.all()
+    # instead of listing all products I will list only those that are available
+    queryset = Product.objects.filter(is_available=True)
+    serializer_class = ProductSerializer
