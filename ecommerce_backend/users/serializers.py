ecd9d8362c202858cfee1ff5b0b66ef561d2877d
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-27 18:44:41 +0700

    fix: user can create account with empty field. Add password validators and password2 for confirmation. Added user registration responseSerializers

--- ecommerce_backend/users/serializers.py
+++ ecommerce_backend/users/serializers.py
@@ -2,6 +2,7 @@
 # defining user serializers to allow log in and sign up for this api 
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password 
 
 # custom user model defined in settings.py (users.CustomUser)
 # with this, am referencing the CustomUser model, and I can do that from any file in the project
@@ -13,11 +14,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     """
     # The password field is write-only for security (cannot be read back from API)
     password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)  # this field is for password confirmation
+
     
     class Meta:
         model = User
         # Fields the user must provide to register
-        fields = ('id', 'email', 'first_name', 'last_name', 'password') 
+        fields = ('id', 'email', 'first_name', 'last_name', 'password', 'password2') 
         # the id is auto generated, and it is read-only
         read_only_fields = ('id',) 
     
@@ -27,6 +30,17 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # 1. Extract the password before creating the user object
         password = validated_data.pop('password', None)
+        validated_data.pop('password2', None) # Remove password2 from validated_data to fix error when creating user
+        
+
+        # There is no password check here! So, users can register with any password they want.
+        # This is a security risk, and it should be fixed.
+        # I will add validation to ensure the password is strong enough.
+        if password:
+            try:
+                validate_password(password) # using Django validators with import
+            except Exception as e:
+                raise serializers.ValidationError({'password': list(e.messages)})
         
         # 2. Create the user object using the remaining validated data
         instance = self.Meta.model(**validated_data)
@@ -38,3 +52,16 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         # 4. Save the new user to the database
         instance.save()
         return instance
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError({"password": "Passwords don't match"})
+        return data
+
+# creating a separate response serializer for user registration
+class UserRegistrationResponseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name')
+    
